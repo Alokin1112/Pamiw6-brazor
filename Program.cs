045c51ef87f9784823b53eae47cd8debecee40d2
Dawid Szczepankowski <dawid.szczepankowski@elzab.pl>
2023-11-15 20:33:40 +0100

    Added services

--- Program.cs
+++ Program.cs
@@ -3,16 +3,21 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Options;
 using Pamiw6;
 using Pamiw6.Shared.Configuration;
+using Pamiw6.Shared.Services.AuthorService;
+using Pamiw6.Shared.Services.BookService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<HttpClient>();
 
 var appSettings = builder.Configuration.GetSection(nameof(AppSettings));
 var appSettingsSection = appSettings.Get<AppSettings>();
 
 builder.Services.AddSingleton<IOptions<AppSettings>>(new OptionsWrapper<AppSettings>(appSettingsSection));
+builder.Services.AddScoped<AuthorService>();
+builder.Services.AddScoped<BookService>();
+
 
 await builder.Build().RunAsync();
